a61a7bdda4bdf70ed620db34d9ac9b7d0d319c3d
Ethan Li <lietk12@gmail.com>
2025-11-12 13:52:01 +0100

    Add boot configs to improve support for the HAT+ and RTC battery (#48)
    This PR partially implements changes suggested in
    https://github.com/openUC2/TechnicalDocs-openUC2-FRAME/issues/161 (as
    part of openUC2 HAT+ enablement) and proposed in
    https://www.notion.so/DN-34-System-time-in-FRAME-machines-2a84e612c78a80569e37d71153c3fc4d?source=copy_link
    (for RTC support).
    
    This PR was done together with @christiankuttke in a
    pair-programming/testing session.

--- os-build/usb-storage/install.sh
+++ os-build/usb-storage/install.sh
@@ -1,8 +1,19 @@
 #!/bin/bash -eux
+# # Note: we need to adjust update-initramfs's behavior to make apt-get finish successfully; see
+# https://github.com/PlanktoScope/PlanktoScope/pull/596 and
+# https://github.com/RPi-Distro/repo/issues/382 for details.
+adjust_initramfs_scope=false
+if grep -q 'MODULES=dep' /etc/initramfs-tools/initramfs.conf; then
+  adjust_initramfs_scope=true
+  sudo sed -i 's~MODULES=dep~MODULES=most~' /etc/initramfs-tools/initramfs.conf
+fi
 sudo apt install -y -o DPkg::Lock::Timeout=60 -o Dpkg::Progress-Fancy=0 \
   udisks2 udisks2-lvm2 \
   exfat-fuse exfatprogs \
   ntfs-3g
+if [ "$adjust_initramfs_scope" = true ]; then
+  sudo sed -i 's~MODULES=most~MODULES=dep~' /etc/initramfs-tools/initramfs.conf
+fi
 
 sudo groupadd storage
 sudo usermod -aG storage "$USER"
